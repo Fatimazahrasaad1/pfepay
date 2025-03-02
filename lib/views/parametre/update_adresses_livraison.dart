6eec446587b8a0d3bf97e4ef0405bbf08660f1ec
Fatima Zahra Saad <saadfatimazahra4@gmail.com>
2025-03-02 15:36:35 +0000

    Correction des erreurs dans update_adresses_livraison.dart

--- lib/views/parametre/update_adresses_livraison.dart
+++ lib/views/parametre/update_adresses_livraison.dart
@@ -10,7 +10,7 @@ class UpdateAdressesLivraisonPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final adresseProvider = context.watch<AdresseProvider>();
+    final adresseProvider = Provider.of<AdresseProvider>(context);
 
     return Scaffold(
       backgroundColor: Colors.white,
