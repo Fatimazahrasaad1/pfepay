9c4f40c0af4f63f6efa30c8107b80c90ac6fe532
Fatima Zahra Saad <saadfatimazahra4@gmail.com>
2025-03-02 14:27:06 +0000

    Your commit message describing the changes

--- lib/views/parametre/update_adresses_livraison.dart
+++ lib/views/parametre/update_adresses_livraison.dart
@@ -10,100 +10,103 @@ class UpdateAdressesLivraisonPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final adresseProvider = Provider.of<AdresseProvider>(context);
+    // Utilisez le widget Consumer pour écouter les changements dans le provider
+    return Consumer<AdresseProvider>(
+      builder: (context, adresseProvider, child) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.green,
+            elevation: 0,
+            title: Text(
+              'Modifier l\'adresse de livraison',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Champ Adresse
+                    TextFormField(
+                      controller: _addressController,
+                      decoration: _inputDecoration('Adresse', Icons.location_on),
+                      validator: (value) =>
+                          value!.isEmpty ? 'Veuillez entrer une adresse' : null,
+                    ),
+                    SizedBox(height: 20),
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        elevation: 0,
-        title: Text(
-          'Modifier l\'adresse de livraison',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Champ Adresse
-                TextFormField(
-                  controller: _addressController,
-                  decoration: _inputDecoration('Adresse', Icons.location_on),
-                  validator: (value) =>
-                      value!.isEmpty ? 'Veuillez entrer une adresse' : null,
-                ),
-                SizedBox(height: 20),
+                    // Champ Ville
+                    TextFormField(
+                      controller: _cityController,
+                      decoration: _inputDecoration('Ville', Icons.location_city),
+                      validator: (value) =>
+                          value!.isEmpty ? 'Veuillez entrer une ville' : null,
+                    ),
+                    SizedBox(height: 20),
 
-                // Champ Ville
-                TextFormField(
-                  controller: _cityController,
-                  decoration: _inputDecoration('Ville', Icons.location_city),
-                  validator: (value) =>
-                      value!.isEmpty ? 'Veuillez entrer une ville' : null,
-                ),
-                SizedBox(height: 20),
+                    // Champ Code Postal
+                    TextFormField(
+                      controller: _postalCodeController,
+                      decoration: _inputDecoration('Code Postal', Icons.pin),
+                      validator: (value) =>
+                          value!.isEmpty ? 'Veuillez entrer un code postal' : null,
+                    ),
+                    SizedBox(height: 30),
 
-                // Champ Code Postal
-                TextFormField(
-                  controller: _postalCodeController,
-                  decoration: _inputDecoration('Code Postal', Icons.pin),
-                  validator: (value) =>
-                      value!.isEmpty ? 'Veuillez entrer un code postal' : null,
-                ),
-                SizedBox(height: 30),
+                    // Bouton Sauvegarder
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            adresseProvider.updateAdresse(
+                              _addressController.text,
+                              _cityController.text,
+                              _postalCodeController.text,
+                            );
 
-                // Bouton Sauvegarder
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        adresseProvider.updateAdresse(
-                          _addressController.text,
-                          _cityController.text,
-                          _postalCodeController.text,
-                        );
-
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content:
-                                Text('Adresse de livraison mise à jour avec succès!'),
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content:
+                                    Text('Adresse de livraison mise à jour avec succès!'),
+                              ),
+                            );
+                          }
+                        },
+                        child: Padding(
+                          padding:
+                              const EdgeInsets.symmetric(horizontal: 20.0, vertical: 12.0),
+                          child: Text(
+                            'Sauvegarder',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
                           ),
-                        );
-                      }
-                    },
-                    child: Padding(
-                      padding:
-                          const EdgeInsets.symmetric(horizontal: 20.0, vertical: 12.0),
-                      child: Text(
-                        'Sauvegarder',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          elevation: 8,
+                          minimumSize: Size(150, 50),
                         ),
                       ),
                     ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                      elevation: 8,
-                      minimumSize: Size(150, 50),
-                    ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 
