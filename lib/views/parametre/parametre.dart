0d8550149e5c334b0530630bf1dd6a303270e9e0
Fatima Zahra Saad <saadfatimazahra4@gmail.com>
2025-03-02 15:02:09 +0000

    Correction des erreurs dans update_adresses_livraison.dart

--- lib/views/parametre/parametre.dart
+++ lib/views/parametre/parametre.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart'; // Import nécessaire pour ChangeNotifierProvider et Provider
 import 'package:mkadia/views/parametre/paiementsave.dart';
 import 'package:mkadia/views/home/HomeView.dart';
 import 'package:mkadia/views/parametre/update_adresses_livraison.dart' as livraison;
 import 'package:mkadia/views/parametre/change_mot_de_pass.dart';
 import 'package:mkadia/views/parametre/gerer_la_confidentialité.dart' as confidentialite;
 import 'package:mkadia/views/profil/profil.dart';
+import 'package:mkadia/provider/adresseprovider.dart';
 
 class ParametrePage extends StatefulWidget {
-  const ParametrePage({Key? key}) : super(key: key); // Ajout du paramètre key
+  const ParametrePage({Key? key}) : super(key: key);
 
   @override
   ParametrePageState createState() => ParametrePageState();
@@ -68,7 +70,7 @@ class ParametrePageState extends State<ParametrePage> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) =>  PaymentSavePage(),
+                        builder: (context) => PaymentSavePage(),
                       ),
                     );
                   },
@@ -86,7 +88,10 @@ class ParametrePageState extends State<ParametrePage> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) =>  livraison.UpdateAdressesLivraisonPage(),
+                        builder: (context) => ChangeNotifierProvider.value(
+                          value: Provider.of<AdresseProvider>(context, listen: false),
+                          child: livraison.UpdateAdressesLivraisonPage(), // Correction ici
+                        ),
                       ),
                     );
                   },
@@ -104,7 +109,7 @@ class ParametrePageState extends State<ParametrePage> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) =>  ChangeMotDePassePage(),
+                        builder: (context) => ChangeMotDePassePage(),
                       ),
                     );
                   },
@@ -116,7 +121,7 @@ class ParametrePageState extends State<ParametrePage> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) =>  confidentialite.GererLaConfidentialitePage(),
+                        builder: (context) => confidentialite.GererLaConfidentialitePage(),
                       ),
                     );
                   },
@@ -131,7 +136,7 @@ class ParametrePageState extends State<ParametrePage> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) =>  ProfilPage(),
+                    builder: (context) => ProfilPage(),
                   ),
                 );
               },
