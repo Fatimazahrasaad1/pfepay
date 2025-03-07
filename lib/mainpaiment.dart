360aad507d5bac47263c8a718227b0e85f535717
Fatima Zahra Saad <saadfatimazahra4@gmail.com>
2025-03-07 18:21:15 +0000

    Correction des erreurs dans mainpaiment.dart

--- lib/mainpaiment.dart
+++ lib/mainpaiment.dart
@@ -1,23 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:mkadia/provider/provider_payment.dart'; // Assurez-vous que ce chemin est correct
-import 'package:mkadia/views/paiement/paiement.dart'; // Assurez-vous que ce chemin est correct
+import 'package:mkadia/provider/PaymentManager.dart';
+import 'package:mkadia/views/parametre/paiementsave.dart';
+import 'package:mkadia/views/paiement/paiement.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final PaymentManager paymentManager = PaymentManager();
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => PaymentProvider(),
-      child: MaterialApp(
-        title: 'Paiement',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(paymentManager: paymentManager),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final PaymentManager paymentManager;
+  HomePage({required this.paymentManager});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Accueil'), backgroundColor: Colors.green),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          PaymentPage(paymentManager: paymentManager)),
+                );
+              },
+              child: Text('Aller à la page Paiement'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          PaymentSavePage(paymentManager: paymentManager)),
+                );
+              },
+              child: Text('Aller à la page Enregistrement Paiement'),
+            ),
+          ],
         ),
-        home: PaymentPage(),
       ),
     );
   }
