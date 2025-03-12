e7aedb536e8b43edb3f62007c268b14d7984dfdb
Fatima Zahra Saad <saadfatimazahra4@gmail.com>
2025-03-12 15:58:56 +0000

    Ajout de nouvelles fonctionnalités et suppression

--- lib/views/paiement/paiement.dart
+++ lib/views/paiement/paiement.dart
@@ -1,101 +1,104 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:mkadia/provider/PaymentManager.dart';
 
 class PaymentPage extends StatelessWidget {
-  const PaymentPage({super.key});
+  final PaymentManager paymentManager;
+
+  const PaymentPage({super.key, required this.paymentManager});
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<PaymentManager>(
-      builder: (context, paymentManager, child) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Paiement'),
-            backgroundColor: Colors.green,
-            elevation: 0,
-          ),
-          body: SingleChildScrollView(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Paiement'),
+        backgroundColor: Colors.green,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Adresse de Livraison',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey[700]),
+            ),
+            const SizedBox(height: 8),
+            Card(
+              elevation: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  '${paymentManager.paymentInfo.address}, ${paymentManager.paymentInfo.city}, ${paymentManager.paymentInfo.country}',
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ),
+            ),
+            const Divider(),
+            const SizedBox(height: 16),
+
+            Text(
+              'Méthode de Paiement',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey[700]),
+            ),
+            const SizedBox(height: 8),
+
+            Card(
+              elevation: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Sélectionnez une méthode de paiement:',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 8),
+                    Wrap(
+                      spacing: 10,
+                      children: ['Carte de Crédit', 'PayPal', 'Apple Pay', 'Google Pay'].map((method) {
+                        return ElevatedButton(
+                          onPressed: () {
+                            paymentManager.setSelectedPaymentMethod(method);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: paymentManager.selectedPaymentMethod == method ? Colors.green : Colors.grey[300],
+                            foregroundColor: paymentManager.selectedPaymentMethod == method ? Colors.white : Colors.black,
+                          ),
+                          child: Text(method),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            if (paymentManager.selectedPaymentMethod == 'Carte de Crédit') ...[
+              _buildCreditCardForm(paymentManager),
+            ] else if (paymentManager.selectedPaymentMethod == 'PayPal') ...[
+              _buildPayPalForm(paymentManager),
+            ] else if (paymentManager.selectedPaymentMethod == 'Apple Pay') ...[
+              _buildApplePayForm(paymentManager),
+            ] else if (paymentManager.selectedPaymentMethod == 'Google Pay') ...[
+              _buildGooglePayForm(paymentManager),
+            ],
+
+            const SizedBox(height: 16),
+
+            // Bouton aligné à droite
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Text(
-                  'Adresse de Livraison',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey[700]),
-                ),
-                const SizedBox(height: 8),
-                Card(
-                  elevation: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Text(
-                      '${paymentManager.paymentInfo.address}, ${paymentManager.paymentInfo.city}, ${paymentManager.paymentInfo.country}',
-                      style: const TextStyle(fontSize: 16),
-                    ),
-                  ),
-                ),
-                const Divider(),
-                const SizedBox(height: 16),
-
-                Text(
-                  'Méthode de Paiement',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey[700]),
-                ),
-                const SizedBox(height: 8),
-
-                Card(
-                  elevation: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Sélectionnez une méthode de paiement:',
-                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: 8),
-                        Wrap(
-                          spacing: 10,
-                          children: ['Carte de Crédit', 'PayPal', 'Apple Pay', 'Google Pay'].map((method) {
-                            return ElevatedButton(
-                              onPressed: () {
-                                paymentManager.setSelectedPaymentMethod(method);
-                              },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: paymentManager.selectedPaymentMethod == method ? Colors.green : Colors.grey[300],
-                                foregroundColor: paymentManager.selectedPaymentMethod == method ? Colors.white : Colors.black,
-                              ),
-                              child: Text(method),
-                            );
-                          }).toList(),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-
-                if (paymentManager.selectedPaymentMethod == 'Carte de Crédit') ...[
-                  _buildCreditCardForm(paymentManager),
-                ] else if (paymentManager.selectedPaymentMethod == 'PayPal') ...[
-                  _buildPayPalForm(paymentManager),
-                ] else if (paymentManager.selectedPaymentMethod == 'Apple Pay') ...[
-                  _buildApplePayForm(paymentManager),
-                ] else if (paymentManager.selectedPaymentMethod == 'Google Pay') ...[
-                  _buildGooglePayForm(paymentManager),
-                ],
-
-                const SizedBox(height: 16),
-
                 ElevatedButton(
                   onPressed: () {
                     paymentManager.savePaymentInfo(context);
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.green,
-                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(8),
                     ),
@@ -104,9 +107,9 @@ class PaymentPage extends StatelessWidget {
                 ),
               ],
             ),
-          ),
-        );
-      },
+          ],
+        ),
+      ),
     );
   }
 
